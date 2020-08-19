d96d7b926cd8e22abdf1d48225924f4633d5327c
arindam roychoudhury <roychoud@hrl.uni-bonn.de>
2020-08-19 23:08:39 +0200

    Swagger UI works now. Need to add more descriptions to the API.

--- customer-service/src/main/java/com/adex/customerservice/configuration/SwaggerConfiguration.java
+++ customer-service/src/main/java/com/adex/customerservice/configuration/SwaggerConfiguration.java
@@ -3,18 +3,19 @@
  */
 package com.adex.customerservice.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.SecurityConfiguration;
-import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Configuration for the Swagger API documentation.
@@ -23,32 +24,37 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  *
  */
 @Configuration
-@EnableSwagger2
-public class SwaggerConfiguration extends WebMvcConfigurationSupport{
+public class SwaggerConfiguration implements WebMvcConfigurer{
+	private final String baseUrl;
 
+	public SwaggerConfiguration(
+			@Value("${springfox.documentation.swagger-ui.base-url:}") String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		String baseUrl = StringUtils.trimTrailingCharacter(this.baseUrl, '/');
+		registry.
+		addResourceHandler(baseUrl + "/swagger-ui/**")
+		.addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+		.resourceChain(false);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController(baseUrl + "/swagger-ui/")
+		.setViewName("forward:" + baseUrl + "/swagger-ui/index.html");
+	}
+	
     @Bean
-    public Docket productApi() {
+    public Docket customerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
+                .select()                 
                 .apis(RequestHandlerSelectors.basePackage("com.adex.customerservice.controller"))
-                .paths(PathSelectors.regex("/customer.*"))
+                .paths(PathSelectors.regex(".*/customer.*"))
                 .build();
              
     }
-    
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
 
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-    
-    @Bean
-    public SecurityConfiguration securityConfiguration() {
-      return SecurityConfigurationBuilder.builder()
-          .enableCsrfSupport(true)
-          .build();
-    }
 }
