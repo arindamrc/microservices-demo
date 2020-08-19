6f15045c80bc1b9ef61f952be08c2b8513e71367
Arindam Roychoudhury <roychoud@hrl.uni-bonn.de>
2020-08-19 15:21:11 +0200

    Started with swagger configuration. But swagger UI is not working.

---
+++ customer-service/src/main/java/com/adex/customerservice/configuration/SwaggerConfiguration.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.adex.customerservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Configuration for the Swagger API documentation.
+ * 
+ * @author arc
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration extends WebMvcConfigurationSupport{
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.adex.customerservice.controller"))
+                .paths(PathSelectors.regex("/customer.*"))
+                .build();
+             
+    }
+    
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+    
+    @Bean
+    public SecurityConfiguration securityConfiguration() {
+      return SecurityConfigurationBuilder.builder()
+          .enableCsrfSupport(true)
+          .build();
+    }
+}
